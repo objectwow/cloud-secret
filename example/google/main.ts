8bfb10548bcb535b1fc53ce5385bd71cae555f57
vtuanjs <vantuan130393@gmail.com>
2025-03-26 13:28:40 +0700

    feat: add sample

---
+++ example/google/main.ts
@@ -0,0 +1,33 @@
+import { SecretManager, GoogleProvider } from "../../src";
+
+async function loadEnvFile() {
+  // You need to install devDependencies - @google-cloud/secret-manager
+  // npm i -D @google-cloud/secret-manager
+  const secretManager = new SecretManager(
+    new GoogleProvider({
+      projectId: process.env.PROJECTID,
+      secretId: process.env.SECRET_ID,
+    }),
+    {
+      hashKey: "your_device_info",
+      useCache: true,
+      // On the server, you can set environment variables directly using the shell,
+      // so there is no need to enable this.
+      // Set `enable` to `false` or an empty string ("") to disable it.
+      enable: process.env.IS_ENABLE_CLOUD_SECRET ? true : false,
+    }
+  );
+
+  await secretManager.loadENV(["REDIS_PASSWORD", "KAFKA_PASS"]);
+}
+
+async function main() {
+  await loadEnvFile();
+
+  console.log(process.env.REDIS_PASSWORD);
+  console.log(process.env.KAFKA_PASS);
+
+  // Your start application logic
+}
+
+main();
